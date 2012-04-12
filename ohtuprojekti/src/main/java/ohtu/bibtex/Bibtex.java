a00db85db33d596cbc93bb678b792548c17d9dab
Anis Moubarik <moubarik@cs.helsinki.fi>
2012-04-12 13:45:10 +0300

    Bibtex muokattu toimimaan Viite luokan kanssa, luokassa vielä turhaa roskaa; pitää siistiä.

--- ohtuprojekti/src/main/java/ohtu/bibtex/Bibtex.java
+++ ohtuprojekti/src/main/java/ohtu/bibtex/Bibtex.java
@@ -17,19 +17,28 @@ public class Bibtex{
     }
     
     public void printAsBibtex(){
+        System.out.println("@"+viite.getType()+"{");
         for (int i = 0; i < viite.getPakollisetKentat().size(); i++) {
-            System.out.println(viite.getPakollisetKentat().get(i));
+            System.out.println("\t"+viite.getPakollistenKenttienNimet().get(i) +" = "
+                    +"\""+viite.getPakollisetKentat().get(i)+"\"");
             
         }
+        System.out.println("}");
     }
     
     public static void main(String[] args) {
         ArrayList pakollisetKentat = new ArrayList<String>();
+        ArrayList pakollistenKenttienNimet = new ArrayList<String>();
         pakollisetKentat.add("nimi");
         pakollisetKentat.add("TestiTitle");
         pakollisetKentat.add("TestJournal");
         pakollisetKentat.add("1994");
-        Viite viite = new Viite("article", "W06", pakollisetKentat, new ArrayList<String>());
+        pakollistenKenttienNimet.add("author");
+        pakollistenKenttienNimet.add("title");
+        pakollistenKenttienNimet.add("journal");
+        pakollistenKenttienNimet.add("year");
+        Viite viite = new Viite("article", "W06", pakollisetKentat, new ArrayList<String>(),
+                pakollistenKenttienNimet, new ArrayList<String>());
         Bibtex bt = new Bibtex(viite);
         bt.printAsBibtex();
     }
