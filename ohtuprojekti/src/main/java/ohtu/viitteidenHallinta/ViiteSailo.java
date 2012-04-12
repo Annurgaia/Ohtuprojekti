7a221998dd36f1a984c31f493af889a9fddf2c61
iimakis <irina.makipaja@helsinki.fi>
2012-04-13 02:32:46 +0300

    viitesäiliö

---
+++ ohtuprojekti/src/main/java/ohtu/viitteidenHallinta/ViiteSailo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.viitteidenHallinta;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author iimakis
+ */
+public class ViiteSailo {
+    private ArrayList<Viite> viitteet;
+    
+    public ViiteSailo() {
+        this.viitteet = new ArrayList<Viite>();
+    }
+    
+    public void addViite() {
+        
+    }
+    
+    public void muokkaaViitetta() {
+        
+    }
+    
+    public ArrayList<Viite> getViitteet() {
+        return viitteet;
+    }
+    
+    public String listaaViitteet() {
+        return "";
+    }
+}
