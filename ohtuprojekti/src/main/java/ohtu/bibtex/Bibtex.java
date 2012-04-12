4202cd785c1da8e799ae888883d669ddafdf716f
Anis Moubarik <moubarik@cs.helsinki.fi>
2012-04-12 20:40:20 +0300

    Bibtex testit ja luokka siistitty.

--- ohtuprojekti/src/main/java/ohtu/bibtex/Bibtex.java
+++ ohtuprojekti/src/main/java/ohtu/bibtex/Bibtex.java
@@ -1,45 +1,50 @@
 package ohtu.bibtex;
-import java.util.ArrayList;
-import static ohtu.viitteidenHallinta.KenttatiedonAlustaja.alustaPakollisetKentat;
-import static ohtu.viitteidenHallinta.KenttatiedonAlustaja.alustaVapaaehtoisetKentat;
-import ohtu.viitteidenHallinta.Viite;
 import ohtu.viitteidenHallinta.ViiteInterface;
-import ohtu.viitteidenHallinta.KenttatiedonAlustaja;
 
 public class Bibtex{
 
 
     ViiteInterface viite;
-    KenttatiedonAlustaja alustaja;
-    
+        
     public Bibtex(ViiteInterface viite){
         this.viite = viite;
     }
     
     public void printAsBibtex(){
-        System.out.println("@"+viite.getType()+"{");
+        System.out.print("@"+viite.getType()+"{"+viite.getId()+",\n");
         for (int i = 0; i < viite.getPakollisetKentat().size(); i++) {
-            System.out.println("\t"+viite.getPakollistenKenttienNimet().get(i) +" = "
-                    +"\""+viite.getPakollisetKentat().get(i)+"\"");
+            System.out.print("\t"+viite.getPakollistenKenttienNimet().get(i) +" = "
+                    +"\""+skanditBibtexiin(viite.getPakollisetKentat().get(i))+"\",\n");
             
         }
-        System.out.println("}");
+        for (int i = 0; i < viite.getVapaaehtoisetKentat().size(); i++) {
+            System.out.print("\t"+viite.getVapaaehtoistenKenttienNimet().get(i) +" = "
+                    +"\""+skanditBibtexiin(viite.getVapaaehtoisetKentat().get(i))+"\",\n");
+        }
+        System.out.print("}");
     }
     
-    public static void main(String[] args) {
-        ArrayList pakollisetKentat = new ArrayList<String>();
-        ArrayList pakollistenKenttienNimet = new ArrayList<String>();
-        pakollisetKentat.add("nimi");
-        pakollisetKentat.add("TestiTitle");
-        pakollisetKentat.add("TestJournal");
-        pakollisetKentat.add("1994");
-        pakollistenKenttienNimet.add("author");
-        pakollistenKenttienNimet.add("title");
-        pakollistenKenttienNimet.add("journal");
-        pakollistenKenttienNimet.add("year");
-        Viite viite = new Viite("article", "W06", pakollisetKentat, new ArrayList<String>(),
-                pakollistenKenttienNimet, new ArrayList<String>());
-        Bibtex bt = new Bibtex(viite);
-        bt.printAsBibtex();
+    private String skanditBibtexiin(String str){
+        String korjattu;
+        korjattu = str.replaceAll("ä", "\\\\\"{a}");
+        korjattu = korjattu.replaceAll("ö", "\\\\\"{o}");
+        korjattu = korjattu.replaceAll("å", "\\\\aa");
+        return korjattu;
+    }
+    
+    public String getAsBibtex(){
+        String bibtex;
+        bibtex = "@"+viite.getType()+"{"+viite.getId()+",\n";
+        for (int i = 0; i < viite.getPakollisetKentat().size(); i++) {
+            bibtex += "\t"+viite.getPakollistenKenttienNimet().get(i) +" = "
+                    +"\""+skanditBibtexiin(viite.getPakollisetKentat().get(i))+"\",\n";
+            
+        }
+        for (int i = 0; i < viite.getVapaaehtoisetKentat().size(); i++) {
+            bibtex += "\t"+viite.getVapaaehtoistenKenttienNimet().get(i) +" = "
+                    +"\""+skanditBibtexiin(viite.getVapaaehtoisetKentat().get(i))+"\",\n";
+        }
+        bibtex += "}";
+        return bibtex;
     }
 }
--- ohtuprojekti/src/test/java/ohtu/bibtex/BibtexTest.java
+++ ohtuprojekti/src/test/java/ohtu/bibtex/BibtexTest.java
@@ -1,7 +1,9 @@
 package ohtu.bibtex;
-import ohtu.viitteidenHallinta.ViiteInterface;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import ohtu.viitteidenHallinta.Viite;
+import ohtu.viitteidenHallinta.ViiteInterface;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,12 +11,48 @@ import org.junit.Test;
 public class BibtexTest{
     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     Bibtex bt;
-    ViiteInterface viite;
     
     @Before
     public void setUp(){
-        bt = new Bibtex(viite);
+        ArrayList pakollisetKentat = new ArrayList<String>();
+        ArrayList pakollistenKenttienNimet = new ArrayList<String>();
+        ArrayList vapaaehtoisetKentat = new ArrayList<String>();
+        ArrayList vapaaehtoistenKenttienNimet = new ArrayList<String>();
+        pakollisetKentat.add("nimiö");
+        pakollisetKentat.add("TestiTitleå");
+        pakollisetKentat.add("TestJournalä");
+        pakollisetKentat.add("1994");
+        pakollistenKenttienNimet.add("author");
+        pakollistenKenttienNimet.add("title");
+        pakollistenKenttienNimet.add("journal");
+        pakollistenKenttienNimet.add("year");
+        vapaaehtoisetKentat.add("5");
+        vapaaehtoisetKentat.add("18");
+        vapaaehtoistenKenttienNimet.add("number");
+        vapaaehtoistenKenttienNimet.add("pages");
+        
+        Viite viites = new Viite("article", "W06", pakollisetKentat, vapaaehtoisetKentat,
+                pakollistenKenttienNimet, vapaaehtoistenKenttienNimet);
+        bt = new Bibtex(viites);
         System.setOut(new PrintStream(outContent));
     }
     
+    @Test
+    public void testPrintAsBibtex(){
+        bt.printAsBibtex();
+        assertEquals(bt.getAsBibtex(), outContent.toString());
+    }
+    
+    @Test
+    public void testGetAsBibtex(){
+        String exp = "@article{W06,\n"
+                + "\tauthor = \"nimi\\\"{o}\",\n"
+                + "\ttitle = \"TestiTitle\\aa\",\n"
+                + "\tjournal = \"TestJournal\\\"{a}\",\n"
+                + "\tyear = \"1994\",\n"
+                + "\tnumber = \"5\",\n"
+                + "\tpages = \"18\",\n"
+                + "}";
+        assertEquals(bt.getAsBibtex(), exp);
+    }    ViiteInterface viite;
 }
