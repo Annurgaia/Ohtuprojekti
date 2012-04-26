fa49cb8b7bb2bf3bb9b3230442a726c472d00d94
Aleksi Majander <aleksi.majander@cs.helsinki.fi>
2012-04-26 15:15:26 +0300

    ListMenu created

---
+++ ohtuprojekti/src/main/java/ohtu/GUI/ListMenu.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.GUI;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import javax.swing.*;
+import ohtu.viitteidenHallinta.*;
+
+/**
+ *
+ * @author Wampie
+ */
+public class ListMenu implements ActionListener{
+
+    ViiteHallinta hallinta;
+    
+    public ListMenu() {
+        
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
