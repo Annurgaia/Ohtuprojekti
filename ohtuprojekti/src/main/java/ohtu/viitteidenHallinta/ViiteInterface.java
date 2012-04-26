00d50682af0d6c03e26d8b9b53d53d851848d2dd
iimakis <irina.makipaja@helsinki.fi>
2012-04-26 18:34:19 +0300

    duhn

--- ohtuprojekti/src/main/java/ohtu/viitteidenHallinta/ViiteInterface.java
+++ ohtuprojekti/src/main/java/ohtu/viitteidenHallinta/ViiteInterface.java
@@ -15,11 +15,7 @@ public interface ViiteInterface {
     String getId();
     String getType();
     LinkedHashMap <String, String> getPakollisetKentat();
-<<<<<<< HEAD
     LinkedHashMap <String, String> getVapaaehtoisetKentat();
-=======
-    LinkedHashMap <String, String> getVapaaehtoisetKentat();;
->>>>>>> 362ed0cccad2f50b96bc276497d478991cb2f54b
     void setPakollisetKentat(LinkedHashMap <String, String> pakollisetKentat);
     void setVapaaehtoisetKentat(LinkedHashMap <String, String> vapaaehtoisetKentat);
 }
