86caf419693ab8345f1e94ad3c971d1c2ec266db
Aleksi Majander <aleksi.majander@cs.helsinki.fi>
2012-04-26 15:34:15 +0300

    Korjattu yksi nullPointerEx

--- ohtuprojekti/src/main/java/ohtu/viitteidenHallinta/ViiteSailo.java
+++ ohtuprojekti/src/main/java/ohtu/viitteidenHallinta/ViiteSailo.java
@@ -7,62 +7,69 @@ package ohtu.viitteidenHallinta;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
-public class ViiteSailo implements ViiteSailoInterface{
+public class ViiteSailo implements ViiteSailoInterface {
+
     private ArrayList<ViiteInterface> viitteet;
-    
+
     public ViiteSailo() {
         this.viitteet = new ArrayList<ViiteInterface>();
     }
-    
+
     public ViiteSailo(ArrayList<ViiteInterface> viitteet) {
-        this.viitteet = viitteet;
+        if (viitteet == null) {
+            this.viitteet = new ArrayList<ViiteInterface>();
+        } else {
+            this.viitteet = viitteet;
+        }
     }
-    
+
     public void addViite(ViiteInterface viite) {
         viitteet.add(viite);
     }
-    
-    public boolean muokkaaViitetta(String id, LinkedHashMap <String, String> pakollisetKentat,
-            LinkedHashMap <String, String> vapaaehtoisetKentat) {
+
+    public boolean muokkaaViitetta(String id, LinkedHashMap<String, String> pakollisetKentat,
+            LinkedHashMap<String, String> vapaaehtoisetKentat) {
         int indeksi = etsiViite(id);
-        if (indeksi == -1)
+        if (indeksi == -1) {
             return false;
+        }
         viitteet.get(indeksi).setPakollisetKentat(pakollisetKentat);
         viitteet.get(indeksi).setVapaaehtoisetKentat(vapaaehtoisetKentat);
         return true;
     }
-    
+
     private int etsiViite(String id) {
         for (int i = 0; i < viitteet.size(); i++) {
-            if (viitteet.get(i).getId().equals(id))
+            if (viitteet.get(i).getId().equals(id)) {
                 return i;
+            }
         }
         return -1;
     }
-    
+
     public ViiteInterface haeViite(String id) {
         for (int i = 0; i < viitteet.size(); i++) {
-            if (viitteet.get(i).getId().equals(id))
+            if (viitteet.get(i).getId().equals(id)) {
                 return viitteet.get(i);
+            }
         }
         return null;
     }
-    
+
     public ArrayList<ViiteInterface> getViitteet() {
         return viitteet;
     }
-    
+
     public String listaaViitteet() {
         String viitelista = "";
-        for (int i = 0; i< viitteet.size(); i++) {
+        for (int i = 0; i < viitteet.size(); i++) {
             viitelista += viitteet.get(i).toString();
             viitelista += "\n";
         }
         return viitelista;
     }
-    
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return viitteet.isEmpty();
     }
 }
- 
