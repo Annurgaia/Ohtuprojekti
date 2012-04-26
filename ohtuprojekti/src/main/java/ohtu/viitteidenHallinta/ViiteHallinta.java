b299d1e662b09d8ffc674c9c853835011aef7c96
iimakis <irina.makipaja@helsinki.fi>
2012-04-27 01:28:21 +0300

    tagaysta

--- ohtuprojekti/src/main/java/ohtu/viitteidenHallinta/ViiteHallinta.java
+++ ohtuprojekti/src/main/java/ohtu/viitteidenHallinta/ViiteHallinta.java
@@ -26,6 +26,12 @@ public class ViiteHallinta {
         sailo = new ViiteSailo(tallennetutViitteet);
         viitelaskuri = 0;
         tag = new TaginHallinta();
+        if (tallennetutViitteet != null) {
+            for (String id : tallennetutViitteet.keySet()) {
+                if (tallennetutViitteet.get(id).getTagit() != null)
+                    tag.lisaaTageihinViite(tallennetutViitteet.get(id), tallennetutViitteet.get(id).getTagit());
+            }
+       }
     }
     
     public ViiteHallinta(Bibtex bibtex, ViitetyyppienKenttainformaatio info, 
@@ -101,84 +107,3 @@ public class ViiteHallinta {
     }
 }
 
-
-///*
-// * To change this template, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package ohtu.viitteidenHallinta;
-//
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.LinkedHashMap;
-//import java.util.Scanner;
-//import ohtu.bibtex.Bibtex;
-//import ohtu.fileIO.ViiteIO;
-//
-///**
-// *
-// * @author Wampie
-// */
-//public class ViiteHallinta {
-//
-//    private static TestiSyote test = new TestiSyote();
-//    private static ViitetyyppienKenttainformaatio info = new ViitetyyppienKenttainformaatio();
-//    private static ViiteTarkistin tarkkailija = new ViiteTarkistin();
-//    private static ViiteSailoInterface sailo = new ViiteSailo();
-//    private static Bibtex bt;
-//    private static String id = "1";
-//
-//    public static void main(String[] args) throws IOException {
-//        Scanner sc = new Scanner(System.in);
-//        while (true) {
-//            switch (test.alkumenu()) {
-//                case 1:
-//                    int temp;
-//                    while (true) {
-//                        while ((temp = test.kysyViitetyyppi(info.getViiteTyypit())) == -1) {
-//                            continue;
-//                        }
-//                        String nimi = info.getViiteTyypit().get(temp - 1);
-//                        LinkedHashMap<String, String> pakolliset = test.kysyPakollisetKentat(info.getTyypinPakollisetKentat(nimi));
-//                        LinkedHashMap<String, String> vapaaehtoiset = test.kysyVapaaehtoisetKentat(info.getTyypinVapaaehtoisetKentat(nimi));
-//                        if (!tarkkailija.tarkistaPakolliset(pakolliset)) {
-//                            continue;
-//                        }
-//                        Viite viite = new Viite(nimi, id, pakolliset, vapaaehtoiset);
-//                        sailo.addViite(viite);
-//                        id = "" + Integer.parseInt(id) + 1;
-//                        break;
-//                    }
-//                    continue;
-//                case 2:
-//                    while (true) {
-//                        String id = test.kysyId();
-//                        ViiteInterface viite;
-//                        if ((viite = sailo.haeViite(id)) == null) {
-//                            System.out.println("Viitettä ei löydy");
-//                            continue;
-//                        }
-//                        LinkedHashMap<String, String> pakolliset = test.kysyPakollisetKentat(viite.getPakollisetKentat());
-//                        LinkedHashMap<String, String> vapaaehtoiset = test.kysyVapaaehtoisetKentat(viite.getVapaaehtoisetKentat());
-//                        if (!tarkkailija.tarkistaPakolliset(pakolliset)) {
-//                            continue;
-//                        }
-//                        sailo.muokkaaViitetta(id, pakolliset, vapaaehtoiset);
-//                        break;
-//                    }
-//                    continue;
-//                case 3:
-//                    System.out.println(sailo.listaaViitteet());
-//                    continue;
-//
-//                case 4:
-//                    break;
-//                default:
-//                    continue;
-//            }
-//            break;
-//
-//        }
-//    }
-//}
--- ohtuprojekti/src/test/easyb/viitteen_lisaaminen.story
+++ ohtuprojekti/src/test/easyb/viitteen_lisaaminen.story
@@ -8,14 +8,13 @@ description 'Käyttäjä voi lisätä viitteen järjestelmään'
 scenario "käyttäjän voi onnistuneesti lisätä viitteen järjestelmään", {
     given 'valitaan viitteen lisääminen', {
        hallinta = new ViiteHallinta()
-       viite = new Viite("tyyppi", "id", new ArrayList<String> tagit, new LinkedHashMap<String, String>(), new LinkedHashMap<String, String>())
-    }
+   }
     when 'Oikeat tiedot syötetään järjestelmään', {
-     hallinta.lisaaViite(viite)
+     hallinta.lisaaViite("asdf", "", new LinkedHashMap<String, String>(), new LinkedHashMap<String, String>())
     }
 
     then 'viite on lisätty järjestelmään', {
-       hallinta.getViitteet().get(id).getType().shouldBe
+       hallinta.getViiteLista().values().size().shouldBe 1
     }
 }
 
